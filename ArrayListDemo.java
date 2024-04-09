74d4b7429489712afdd83a0f228ae0b59a3baf4b
Vinayak <vinayakdev293@gmail.com>
2024-04-09 18:00:07 -0400

    Implemented 2-d Arraylist

--- ArrayListDemo.java
+++ ArrayListDemo.java
@@ -19,5 +19,28 @@ public class ArrayListDemo{
 		for(int i=0; i<food.size(); i++) {
 			System.out.println(food.get(i));
 		}
+
+		ArrayList<ArrayList<String>> groceryList = new ArrayList();
+		
+		ArrayList<String> bakeryList = new ArrayList<String>();
+		bakeryList.add("pasta");
+		bakeryList.add("garlic bread");
+		bakeryList.add("donuts");
+		
+		ArrayList<String> produceList = new ArrayList<String>();
+		produceList.add("tomatoes");
+		produceList.add("zucchini");
+		produceList.add("peppers");
+		
+		ArrayList<String> drinksList = new ArrayList<String>();
+		drinksList.add("soda");
+		drinksList.add("coffee");
+		
+		groceryList.add(bakeryList);
+		groceryList.add(produceList);
+		groceryList.add(drinksList);
+		
+		System.out.println(groceryList);
+
 	}
 }
