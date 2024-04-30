cc485ac5288530fb4a24c4788c9b85563be76669
Yaksh <ctrlaltdel@myyahoo.com>
2024-04-29 20:05:56 -0400

    Reverse an array without using another array

---
+++ DataStructure/ReverseArray.java
@@ -0,0 +1,24 @@
+package DataStructure;
+
+public class ReverseArray {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5};
+        reverseArray(arr);
+        System.out.println("Reversed array:");
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+
+    public static void reverseArray(int[] arr) {
+        int start = 0;
+        int end = arr.length - 1;
+        while (start < end) {
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
+    }
+}
