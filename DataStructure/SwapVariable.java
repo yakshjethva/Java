65b1c36c3712a778fcfc471a5b1163a4cacb97a5
Yaksh <ctrlaltdel@myyahoo.com>
2024-05-06 21:53:12 -0400

    Swap variable with multiple methods

--- DataStructure/SwapVariable.java
+++ DataStructure/SwapVariable.java
@@ -1,5 +1,49 @@
 package DataStructure;
 
 public class SwapVariable {
-    
+    public static void main(String args[])
+    {
+        int a = 5;
+        int b = 10;
+
+        System.out.println("Before swapping:");
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+        
+        // Method 1: Using a Temporary Variable
+        int temp = a;
+        a = b;
+        b = temp;
+        System.out.println("\nAfter swapping with temporary variable:");
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+
+        // Method 2: Using Arithmetic Operations
+        a = 5;
+        b = 10;
+        a = a + b;
+        b = a - b;
+        a = a - b;
+        System.out.println("\nAfter swapping with arithmetic operations:");
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+        
+        // Method 3: Using XOR Operation
+        a = 5;
+        b = 10;
+        a = a ^ b;
+        b = a ^ b;
+        a = a ^ b;
+        System.out.println("\nAfter swapping with XOR operation:");
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+        
+        // Method 4: Using a Single Statement
+        a = 5;
+        b = 10;
+        b = (a + b) - (a = b);
+        System.out.println("\nAfter swapping with single statement:");
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+    }
 }
