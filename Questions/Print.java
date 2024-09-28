34c82f8a5d8f0dfeb28a0f1fbf33720f75e492ae
Yaksh <ctrlaltdel@myyahoo.com>
2024-09-28 15:45:59 -0400

    Tricky Question

---
+++ Questions/Print.java
@@ -0,0 +1,11 @@
+package Questions;
+
+
+/* Write a print statement without adding any ;  */
+public class Print {
+    public static void main(String args[]){
+        for(int i = 0 ; i<1 ;System.out.println("Hello World")){
+            i++;
+        }
+    }
+}
